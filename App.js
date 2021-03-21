dc8f102a93c17ef855361678835b746217c69bbf
Cr1c4rd0 <cristianricardo87@gmail.com>
2021-03-21 13:40:19 -0500

    Geolocation project use Expo

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Alert, Dimensions, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+  const [location, setLocation] = useState({})
+  const buscaLocation = async () => {
+    const { status } = await Location.requestPermissionsAsync()
+    if (status !== 'granted') {
+      return Alert.alert('No tenemos los permisos necesarios para acceder a la location')
+    }
+    const location = await Location.getCurrentPositionAsync({})
+    setLocation(location)
+  }
+  useEffect (() => {
+    buscaLocation()
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <MapView style={styles.map}>
+       {location.coords
+        ? <Marker
+            coordinate={location.coords}
+            title="Titulo"
+            description="description del punto" />
+        : null
+       }
+     </MapView>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
